12a0269f2a28b593fec27b8e2374c203bb3e3974
Ariel Aguado Albear <ariel.841207@gmail.com>
2023-04-16 23:52:18 -0400

    Show signOut only when user is logged in

--- src/components/NavigationOptions.tsx
+++ src/components/NavigationOptions.tsx
@@ -12,15 +12,17 @@ export default function NavigationOptions({ menu = 'desktop' }) {
     <ul className={ isForMobile ? 'navigation-options mobile' : 'navigation-options desktop' }>
       <li className="navigation-options__switch-theme"><SwitchTheme /></li>
       {user && (
-        <div className="navigation-options__user">
+        <li className="navigation-options__user">
           {user.email.split('@')[0]}
-        </div>
+        </li>
       )}
-      <li className="navigation-options__sign-out">
-        <div className="f-icon-3">
-          <svg width="100%" height="100%" viewBox="0 0 24 24"><path fill="currentColor" d="M4 12a1 1 0 0 0 1 1h7.59l-2.3 2.29a1 1 0 0 0 0 1.42a1 1 0 0 0 1.42 0l4-4a1 1 0 0 0 .21-.33a1 1 0 0 0 0-.76a1 1 0 0 0-.21-.33l-4-4a1 1 0 1 0-1.42 1.42l2.3 2.29H5a1 1 0 0 0-1 1ZM17 2H7a3 3 0 0 0-3 3v3a1 1 0 0 0 2 0V5a1 1 0 0 1 1-1h10a1 1 0 0 1 1 1v14a1 1 0 0 1-1 1H7a1 1 0 0 1-1-1v-3a1 1 0 0 0-2 0v3a3 3 0 0 0 3 3h10a3 3 0 0 0 3-3V5a3 3 0 0 0-3-3Z"></path></svg>
-        </div>
-      </li>
+      {user && (
+        <li className="navigation-options__sign-out">
+          <div className="f-icon-3">
+            <svg width="100%" height="100%" viewBox="0 0 24 24"><path fill="currentColor" d="M4 12a1 1 0 0 0 1 1h7.59l-2.3 2.29a1 1 0 0 0 0 1.42a1 1 0 0 0 1.42 0l4-4a1 1 0 0 0 .21-.33a1 1 0 0 0 0-.76a1 1 0 0 0-.21-.33l-4-4a1 1 0 1 0-1.42 1.42l2.3 2.29H5a1 1 0 0 0-1 1ZM17 2H7a3 3 0 0 0-3 3v3a1 1 0 0 0 2 0V5a1 1 0 0 1 1-1h10a1 1 0 0 1 1 1v14a1 1 0 0 1-1 1H7a1 1 0 0 1-1-1v-3a1 1 0 0 0-2 0v3a3 3 0 0 0 3 3h10a3 3 0 0 0 3-3V5a3 3 0 0 0-3-3Z"></path></svg>
+          </div>
+        </li>
+      )}
       <li className="navigation-options__sign">
         {
           user
