9c942f8a66bb3af9615e60850f305e670df96b40
Ariel Aguado Albear <ariel.841207@gmail.com>
2023-04-16 23:44:10 -0400

    Change the import css file

--- src/main.tsx
+++ src/main.tsx
@@ -4,8 +4,6 @@ import { RouterProvider } from 'react-router-dom'
 import { router } from './routes'
 import AuthProvider from './context/AuthProvider'
 
-import './assets/styles/css/style.css'
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <AuthProvider>
